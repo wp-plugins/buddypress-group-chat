9633f14ddc7241fc40164762f9514b45bb261775
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-13 12:49:41 +0000

    Update of version tested on wordpress 4.0
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-group-chat/trunk@947726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- loader.php
+++ loader.php
@@ -6,14 +6,14 @@
 * Author URI: http://profiles.wordpress.org/ruddernation
 * Description: This plugin is used for BuddyPress to all Group creators to allow the use of TinyChat in groups,
 * The Chat also include youtube/soundcloud for all users, Even if you're a moderator or not.
-* Version: 1.0.6
+* Version: 1.1.6
 * Requires at least: WordPress 3.6.0, BuddyPress 1.8.1
-* Tested up to: WordPress 3.9.1 / BuddyPress 2.0.1
+* Tested up to: WordPress 4.0-beta1 / BuddyPress 2.0.1
 * Site Wide Only: true
-* Date: 10th July 2014
+* Date: 13th July 2014
 * License: GPL
-* License URI: http://www.gnu.org/licenses/gpl-2.0.html
-* Important - Only load the component if BuddyPress is activated! 
+* License URI: http://www.gnu.org/licenses/gpl.html
+* Important - Only load if BuddyPress Groups is activated! 
 */
 
 function bp_tinychat_group_chat_init() {
--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Contributors: ruddernation
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=FFAC7FBEBH6JE
 Tags: group chat, buddypress group chat,video chat, Tinychat, chat,buddypress chat,buddypress video chat
 Requires at least: 3.6.0
-Tested up to: 3.9.1
-Stable tag: 1.0.6
+Tested up to: 4.0
+Stable tag: 1.1.6
 License: GPL
 License URI: http://www.gnu.org/licenses/gpl.html
 
