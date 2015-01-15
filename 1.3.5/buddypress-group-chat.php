a9fccd5a731caff14a389f66952d5cf7cc26ead8
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-15 16:17:13 +0000

    New tag
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-group-chat/trunk@1068698 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.3.5/buddypress-group-chat.php
@@ -0,0 +1,39 @@
+<?php
+/*
+* Plugin Name: BuddyPress Group Chat
+* Plugin URI: https://wordpress.org/plugins/buddypress-group-chat/
+* Author: Ruddernation Designs
+* Author URI: http://profiles.wordpress.org/ruddernation
+* Description: This plugin is used for BuddyPress to all Group creators to allow the use of TinyChat in groups,
+* The Chat also include youtube/soundcloud for all users, Even if you're not a moderator. (This is of no association of The other buddypress group chat! Cheeky Fucker)
+* Version: 1.3.5
+* Requires at least: WordPress 3.6.0, BuddyPress 1.8.1
+* Tested up to: WordPress 4.1, BuddyPress 2.1.1
+* Site Wide Only: true
+* Date: 15th January 2015
+* License: GPLv3
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+* Important - Only load if BuddyPress Groups is activated! 
+*/
+function bp_tinychat_group_chat_init() {
+	require( dirname( __FILE__ ) . '/chat-core.php' );}
+add_action( 'bp_init', 'bp_tinychat_group_chat_init' );
+function bp_tinychat_group_chat_activate() {
+	global $wpdb;
+	if ( !empty($wpdb->charset) )
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+$sql[] = "CREATE TABLE {$wpdb->base_prefix}bp_tinychat_group_chat (
+id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+group_id bigint(20) NOT NULL,
+user_id bigint(20) NOT NULL,
+message_content text) {$charset_collate};";
+	$sql[] = "CREATE TABLE {$wpdb->base_prefix}bp_tinychat_group_chat_online (
+id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+group_id bigint(20) NOT NULL,
+user_id bigint(20) NOT NULL,
+timestamp int(11) NOT NULL) {$charset_collate};";
+	require_once( ABSPATH . 'wp-admin/upgrade-functions.php' );
+	dbDelta($sql);
+	update_site_option( 'bp-tinychat-group-chat-db-version', BP_TINYCHAT_GROUP_CHAT_DB_VERSION );
+}
+register_activation_hook( __FILE__, 'bp_tinychat_group_chat_activate' ); ?>
