f974caed65e445aac9da818f428790b3c433d018
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 10:13:38 +0000

    Version update.
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-group-chat/trunk@1078880 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chat-core.php
+++ chat-core.php
@@ -1,7 +1,7 @@
 <?php
 define ( 'BP_TINYCHAT_GROUP_CHAT_IS_INSTALLED', 1 );
-define ( 'BP_TINYCHAT_GROUP_CHAT_VERSION', '1.3.6' );
-define ( 'BP_TINYCHAT_GROUP_CHAT_DB_VERSION', '1.3.6' );
+define ( 'BP_TINYCHAT_GROUP_CHAT_VERSION', '1.3.7' );
+define ( 'BP_TINYCHAT_GROUP_CHAT_DB_VERSION', '1.3.7' );
 if ( !defined( 'BP_TINYCHAT_GROUP_CHAT_SLUG' ) )
 	define ( 'BP_TINYCHAT_GROUP_CHAT_SLUG', 'Group Chat' );
 function bp_tinychat_group_chat_setup_globals() {
