6af8cdb1d1e94231de948029db373883f0c1bc88
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 09:30:48 +0000

    Major update! This is to fix certain BP_Group_Extension notification errors.
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-group-chat/trunk@1158658 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- buddypress-group-chat.php
+++ buddypress-group-chat.php
@@ -6,18 +6,25 @@
 * Author URI: http://profiles.wordpress.org/ruddernation
 * Description: This plugin is used for BuddyPress to all Group creators to allow the use of TinyChat in groups,
 * The Chat also includes youtube/soundcloud for all users, Even if you're not a moderator.
-* Version: 1.4.8.2
+* Version: 1.4.9
 * Requires at least: WordPress 3.6.0, BuddyPress 1.8.1
 * Tested up to: WordPress 4.2, BuddyPress 2.2.3.1
 * Site Wide Only: true
-* Date: 08th Mayl 2015
+* Date: 12th Mayl 2015
 * License: GPLv3
 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 * Important - Only load if BuddyPress Groups is activated! 
 */
 function bp_tinychat_group_chat_init() {
+	    global $wpdb;
+    if ( is_multisite() && BP_ROOT_BLOG != $wpdb->blogid ) {
+	return;
+    }
+    if ( ! bp_is_active( 'groups' ) ) {
+	return;
+    }
 	require( dirname( __FILE__ ) . '/chat-core.php' );}
-add_action( 'bp_init', 'bp_tinychat_group_chat_init' );
+add_action( 'bp_init', 'bp_tinychat_group_chat_init' , 82);
 function bp_tinychat_group_chat_activate() {
 	global $wpdb;
 	if ( !empty($wpdb->charset) )
--- chat-core.php
+++ chat-core.php
@@ -1,21 +1,17 @@
 <?php
-define ( 'BP_TINYCHAT_GROUP_CHAT_IS_INSTALLED', 1 );
-define ( 'BP_TINYCHAT_GROUP_CHAT_VERSION', '1.4.8.1' );
-define ( 'BP_TINYCHAT_GROUP_CHAT_DB_VERSION', '1.4.8.1' );
-if ( !defined( 'BP_TINYCHAT_GROUP_CHAT_SLUG' ) )
-	define ( 'BP_TINYCHAT_GROUP_CHAT_SLUG', 'Group Chat' );
-function bp_tinychat_group_chat_setup_globals() {
-	global $bp, $wpdb;$bp->tinychat->id = 'tinychat group chat';$bp->tinychat->table_name = $wpdb->base_prefix . 'bp_tinychat_group_chat';$bp->tinychat->format_notification_function = 'bp_tinychat_group_chat_format_notifications';$bp->tinychat->slug = BP_TINYCHAT_GROUP_CHAT_SLUG;$bp->active_components[$bp->tinychat->slug] = $bp->tinychat->id;}
-add_action( 'bp_setup_globals', 'bp_tinychat_group_chat_setup_globals' );
 class BP_tinychat_group_chat extends BP_Group_Extension {	
-	function bp_tinychat_group_chat() {
-		global $bp;$this->name = 'Group Chat';$this->slug = 'group-chat';$this->create_step_position = 48;$this->nav_item_position = 83;
+	public function  __construct() {
+		global $bp;
+		$this->name = 'Group Chat';
+		$this->slug = 'group-chat';
+		$this->create_step_position = 48;
+		$this->nav_item_position = 83;
 		if ( groups_get_groupmeta( $bp->groups->current_group->id, 'bp_tinychat_group_chat_enabled' ) == '1' ) {
 			$this->enable_nav_item = true;
 		} else {
 			$this->enable_nav_item = false;}
 			}
-			function create_screen() {
+public function create_screen($group_id = null) {
 		global $bp;
 		if ( !bp_is_group_creation_step( $this->slug ) )
 			return false;
@@ -24,12 +20,13 @@ class BP_tinychat_group_chat extends BP_Group_Extension {
 			<?php if ( groups_get_groupmeta( $bp->groups->current_group->id, 'bp_tinychat_group_chat_enabled' ) == '1' ) {echo 'checked=1';}?>		/>
         Enable Group Chat
 		<hr>
-		<?php }function create_screen_save() {
+		<?php }
+public function create_screen_save($group_id = null) {
 		global $bp;
 		check_admin_referer( 'groups_create_save_' . $this->slug );	
 		if ( $_POST['bp_tinychat_group_chat_enabled'] == 1 ) {
 			groups_update_groupmeta( $bp->groups->current_group->id, 'bp_tinychat_group_chat_enabled', 1 );}}
-	function edit_screen() {
+public	function edit_screen($group_id = null) {
 		global $bp;
 		if ( !groups_is_user_admin( $bp->loggedin_user->id, $bp->groups->current_group->id ) ) {
 			return false;}	
@@ -42,7 +39,7 @@ class BP_tinychat_group_chat extends BP_Group_Extension {
 		<hr>
 		<input type="submit" name="save" value="Save" />
 		<?php }
-	function edit_screen_save() {
+public	function edit_screen_save($group_id = null) {
 		global $bp;
 		if ( !isset( $_POST['save'] ) )
 			return false;
@@ -53,7 +50,7 @@ class BP_tinychat_group_chat extends BP_Group_Extension {
 			groups_update_groupmeta( $bp->groups->current_group->id, 'bp_tinychat_group_chat_enabled', 0 );}
 		bp_core_add_message( __( 'Settings saved successfully', 'buddypress' ) );
 		bp_core_redirect( bp_get_group_permalink( $bp->groups->current_group ) . 'admin/' . $this->slug );}
-	function display() {
+public	function display($group_id = null) {
 		global $bp;
 		if ( groups_is_user_member( $bp->loggedin_user->id, $bp->groups->current_group->id ) || groups_is_user_mod( $bp->loggedin_user->id, $bp->groups->current_group->id ) || groups_is_user_admin( $bp->loggedin_user->id, $bp->groups->current_group->id ) || is_super_admin() ) {$name=apply_filters( 'bp_get_group_name', $bp->groups->current_group->name );$name=preg_replace('/\s+/','',$name);$name=htmlspecialchars($name,ENT_QUOTES, 'UTF-8');$name=strtolower($name);?>
 			<div id="item-body">
@@ -68,4 +65,5 @@ embed = tinychat({room: "<?php echo $name?>",<?php {echo ' account:"'.$bp->logge
             <?php
 		} }
            else {
-			echo '<div id="message" class="error"><p>Sorry group chat is only available to group members, Please join or request to join the group.</p></div>';}}} bp_register_group_extension( 'BP_tinychat_group_chat' ); ?>
+			echo '<div id="message" class="error"><p>Sorry group chat is only available to group members, Please join or request to join the group.</p></div>';
+			}}} bp_register_group_extension( 'BP_tinychat_group_chat' ); ?>
