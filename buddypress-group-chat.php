92217753fe26993291026b9f288e7230c291d6b6
ruddernation <ruddernation@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 11:45:56 +0000

    yawn.
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-group-chat/trunk@1185490 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ buddypress-group-chat.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Plugin Name: BuddyPress Group Chat - Only On Github
+* Plugin URI: https://wordpress.org/plugins/buddypress-group-chat/
+* Author: wordpress
+* Author URI: http://profiles.wordpress.org/wordpress
+* Description: Now only being updated on Github due to WordPress Moderators thinking they're above the law but not being very bright.
+* Version: 1.5.1
+* Requires at least: WordPress 3.6.0, BuddyPress 1.8.1
+* Tested up to: WordPress 4.3, BuddyPress 2.3
+* License: GPLv3
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+?>
